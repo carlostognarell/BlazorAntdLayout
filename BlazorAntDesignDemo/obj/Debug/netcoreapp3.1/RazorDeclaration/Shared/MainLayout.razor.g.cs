2b2fcd36f885064990345198dd47e58eecba514c
carlostognarell <69538468+carlostognarell@users.noreply.github.com>
2021-09-23 11:07:12 -0300

    commit-newcomponents

--- BlazorAntDesignDemo/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ BlazorAntDesignDemo/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace BlazorAntDesignDemo.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Carlos Tognarell\Downloads\BlazorAntDesignDemo\BlazorAntDesignDemo\BlazorAntDesignDemo\_Imports.razor"
+#line 1 "C:\Users\Carlos Tognarell\Documents\GitHub\BlazorAntdLayout\BlazorAntDesignDemo\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Carlos Tognarell\Downloads\BlazorAntDesignDemo\BlazorAntDesignDemo\BlazorAntDesignDemo\_Imports.razor"
+#line 2 "C:\Users\Carlos Tognarell\Documents\GitHub\BlazorAntdLayout\BlazorAntDesignDemo\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Carlos Tognarell\Downloads\BlazorAntDesignDemo\BlazorAntDesignDemo\BlazorAntDesignDemo\_Imports.razor"
+#line 3 "C:\Users\Carlos Tognarell\Documents\GitHub\BlazorAntdLayout\BlazorAntDesignDemo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Carlos Tognarell\Downloads\BlazorAntDesignDemo\BlazorAntDesignDemo\BlazorAntDesignDemo\_Imports.razor"
+#line 4 "C:\Users\Carlos Tognarell\Documents\GitHub\BlazorAntdLayout\BlazorAntDesignDemo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Carlos Tognarell\Downloads\BlazorAntDesignDemo\BlazorAntDesignDemo\BlazorAntDesignDemo\_Imports.razor"
+#line 5 "C:\Users\Carlos Tognarell\Documents\GitHub\BlazorAntdLayout\BlazorAntDesignDemo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Carlos Tognarell\Downloads\BlazorAntDesignDemo\BlazorAntDesignDemo\BlazorAntDesignDemo\_Imports.razor"
+#line 6 "C:\Users\Carlos Tognarell\Documents\GitHub\BlazorAntdLayout\BlazorAntDesignDemo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Carlos Tognarell\Downloads\BlazorAntDesignDemo\BlazorAntDesignDemo\BlazorAntDesignDemo\_Imports.razor"
+#line 7 "C:\Users\Carlos Tognarell\Documents\GitHub\BlazorAntdLayout\BlazorAntDesignDemo\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Carlos Tognarell\Downloads\BlazorAntDesignDemo\BlazorAntDesignDemo\BlazorAntDesignDemo\_Imports.razor"
+#line 8 "C:\Users\Carlos Tognarell\Documents\GitHub\BlazorAntdLayout\BlazorAntDesignDemo\_Imports.razor"
 using BlazorAntDesignDemo;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Carlos Tognarell\Downloads\BlazorAntDesignDemo\BlazorAntDesignDemo\BlazorAntDesignDemo\_Imports.razor"
+#line 9 "C:\Users\Carlos Tognarell\Documents\GitHub\BlazorAntdLayout\BlazorAntDesignDemo\_Imports.razor"
 using BlazorAntDesignDemo.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Carlos Tognarell\Downloads\BlazorAntDesignDemo\BlazorAntDesignDemo\BlazorAntDesignDemo\_Imports.razor"
+#line 10 "C:\Users\Carlos Tognarell\Documents\GitHub\BlazorAntdLayout\BlazorAntDesignDemo\_Imports.razor"
 using AntDesign;
 
 #line default
@@ -90,7 +90,7 @@ using AntDesign;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 4 "C:\Users\Carlos Tognarell\Downloads\BlazorAntDesignDemo\BlazorAntDesignDemo\BlazorAntDesignDemo\Shared\MainLayout.razor"
+#line 4 "C:\Users\Carlos Tognarell\Documents\GitHub\BlazorAntdLayout\BlazorAntDesignDemo\Shared\MainLayout.razor"
  
     bool collapsed;
 
@@ -103,7 +103,7 @@ using AntDesign;
 #line hidden
 #nullable disable
 #nullable restore
-#line 83 "C:\Users\Carlos Tognarell\Downloads\BlazorAntDesignDemo\BlazorAntDesignDemo\BlazorAntDesignDemo\Shared\MainLayout.razor"
+#line 95 "C:\Users\Carlos Tognarell\Documents\GitHub\BlazorAntdLayout\BlazorAntDesignDemo\Shared\MainLayout.razor"
       
 
     void OnCollapse(bool isCollapsed)
